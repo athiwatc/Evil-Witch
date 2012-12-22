98b61c1792f7d171f4fc9531cd206b32641e4597
peepo3663 <peepo157@gmail.com>
2012-12-22 22:42:59 +0700

    Add action to button

--- src/com/cg/view/StartUI.java
+++ src/com/cg/view/StartUI.java
@@ -31,7 +31,7 @@ import com.sun.xml.internal.ws.org.objectweb.asm.Label;
 
 import jogamp.opengl.glu.mipmap.ExtractUShort;
 
-public class StartUI extends JFrame{
+public class StartUI extends JFrame {
 
 	/**
 	 * 
@@ -42,17 +42,18 @@ public class StartUI extends JFrame{
 	private JLabel showDescription;
 	private String description;
 	private JPanel desJPanel;
-	private ImagePanel	imgPanel;
+	// private ImagePanel imgPanel;
 	private JLabel label;
-	private ButtonGroup btg;
+	// private ButtonGroup btg;
 	private JPanel buttonPanel;
 	private BufferedImage image;
 	private String path = "pic/wicked-witch.jpg";
-	public StartUI () {
+
+	public StartUI() {
 		this.setTitle("Evil Witch");
-		this.setLayout( new BorderLayout());
+		this.setLayout(new BorderLayout());
 		initComponents();
-		this.setSize(800,600);
+		this.setSize(800, 600);
 		this.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent ev) {
 				System.exit(0);
@@ -60,10 +61,12 @@ public class StartUI extends JFrame{
 		});
 		this.pack();
 	}
+
 	public void run() {
 		// TODO Auto-generated method stub
 		this.setVisible(true);
 	}
+
 	public void initComponents() {
 		start = new JButton("Start");
 		exit = new JButton("Exit");
@@ -72,22 +75,24 @@ public class StartUI extends JFrame{
 		description = "This is Evil Witch Game Create by Athiwat and Wasupol Team";
 		showDescription = new JLabel(description);
 		try {
-			image = ImageIO.read(new File(path ));
+			image = ImageIO.read(new File(path));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		label = new JLabel(new ImageIcon(image));
-//		btg = new ButtonGroup();
+		// btg = new ButtonGroup();
 		desJPanel.add(showDescription);
-//		btg.add(start);
-//		btg.add(exit);
+		// btg.add(start);
+		// btg.add(exit);
+		exit.addActionListener(new ExitListener());
 		buttonPanel.add(start);
 		buttonPanel.add(exit);
 		this.add(label, BorderLayout.PAGE_START);
-		this.add(desJPanel,BorderLayout.CENTER);
-		this.add(buttonPanel,BorderLayout.PAGE_END);
+		this.add(desJPanel, BorderLayout.CENTER);
+		this.add(buttonPanel, BorderLayout.PAGE_END);
 	}
+
 	class ExitListener implements ActionListener {
 
 		@Override
@@ -95,6 +100,6 @@ public class StartUI extends JFrame{
 			// TODO Auto-generated method stub
 			System.exit(0);
 		}
-		
+
 	}
 }

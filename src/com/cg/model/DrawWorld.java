5fdd9edbbf8867ea6dce3c8ee5da2701cc439b90
PAPANG <PAPANG@158.108.225.197>
2012-12-22 16:32:28 +0700

    Update Drawing World to be GL Event.

--- src/com/cg/model/DrawWorld.java
+++ src/com/cg/model/DrawWorld.java
@@ -1,5 +1,33 @@
 package com.cg.model;
 
-public class DrawWorld {
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+
+public class DrawWorld implements GLEventListener{
+
+	@Override
+	public void display(GLAutoDrawable arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose(GLAutoDrawable arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GLAutoDrawable arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3,
+			int arg4) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
